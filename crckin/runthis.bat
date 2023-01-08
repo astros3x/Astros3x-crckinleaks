7fbaf712d8812f1d07c320689bb4d70c93edb9b3
2Loop <87500882+astros3x@users.noreply.github.com>
2023-01-08 17:26:59 +0100

    Add files via upload

---
+++ crckin/runthis.bat
@@ -0,0 +1,64 @@
+@echo off
+title Cracking Ref ~2Loop#6969
+color c
+:menu
+type main\title.py
+set /p input= Input here:
+if '%input%'=='1' goto gamez
+if '%input%'=='2' goto softwarez
+if '%input%'=='3' goto chetos
+if '%input%'=='4' goto freechetz
+if '%input%'=='5' goto streamin
+if '%input%'=='6' goto anim
+if '%input%'=='7' goto crackz
+if '%input%'=='8' goto tubz
+if '%input%'=='9' goto dsearch
+if '%input%'=='10' goto wikz
+if '%input%'=='11' goto zpload
+if '%input%'=='12' goto quit
+:gamez
+@echo off & cls
+explorer "https://agfy.co/"
+goto menu
+:softwarez
+@echo off & cls
+explorer "https://filecr.com/"
+goto menu
+:chetos
+@echo off & cls
+explorer "https://ezmod.vip/"
+goto menu
+:freechetz
+@echo off & cls
+explorer "https://www.wemod.com/"
+goto menu
+:streamin
+@echo off & cls
+explorer "https://streamingcommunity.actor/"
+goto menu
+:anim
+@echo off & cls
+explorer "https://animeunit.tv/"
+goto menu
+:crackz
+@echo off & cls
+explorer "https://cracking.org/"
+goto menu
+:tubz
+@echo off & cls
+explorer "https://crax.tube/"
+goto menu
+:dsearch
+@echo off & cls
+explorer "https://ahmia.fi/"
+goto menu
+:wikz
+@echo off & cls
+explorer "https://hidden.wiki/"
+goto menu
+:zpload
+@echo off & cls
+explorer "https://anonfiles.com/"
+goto menu
+:quit
+exit
